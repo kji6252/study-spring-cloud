71e18ebc82fceda95cbe959279519cbaa3b58e50
Jongin Kim <kji6252@sk.com>
2020-05-24 15:27:22 +0900

    Add Cloud Config

--- study-springboot-discoveryclient/src/main/java/io/github/kji6252/studyspringbootdiscoveryclient/ServiceInstanceRestController.java
+++ study-springboot-discoveryclient/src/main/java/io/github/kji6252/studyspringbootdiscoveryclient/ServiceInstanceRestController.java
@@ -1,6 +1,7 @@
 package io.github.kji6252.studyspringbootdiscoveryclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,14 @@ public class ServiceInstanceRestController {
     @GetMapping("/service-instances/{applicationName}")
     public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
         return  discoveryClient.getInstances(applicationName);
+    }
+
+    @Value("${message: default message}")
+    String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
     }
 
 }
